1064f636dbf5812bf60098f745cb31913aecee4b
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2026-02-24 08:37:48 +0200

    feat: main file

---
+++ main.py
@@ -0,0 +1,38 @@
+# TEAM DETAILS
+# Ahmed Abd Al Moneim
+# Details: 
+#       ID: <id here>
+#       Group: 3
+#       Section: 1
+#
+# Andrew Sameh
+# Details:
+#       ID: 9489
+#       Group: 3
+#       Section: 1
+#
+# Galal Mohamed
+# Details:
+#       ID: <id here>
+#       Group: 3
+#       Section: 1
+#
+# -------------------------
+# MAIN PROGRAM
+# Written by: Andrew Sameh
+# Author Details:
+#       ID: 9489
+#       Group: 3
+#       Section: 1
+
+from bubble_sort import bubble_sort
+from selection_sort import selection_sort
+from insertion_sort import insertion_sort
+
+def main():
+    sizes = [200, 500, 1000, 10000]
+    sorts = [bubble_sort, selection_sort, insertion_sort]
+    # Function call for timing here
+
+if (__name__ == "__main__"):
+    main()
