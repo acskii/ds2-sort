1f19c210ef0cd05aaab13dcffc69ea25c1573192
Andrew Sameh <andrew.sameh.adel@gmail.com>
2026-02-25 12:25:16 +0200

    Add insertion sort (#1)
    * feat: insertion sort
    * docs: insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,10 +1,24 @@
 # INSERTION SORT ALGORTIHM
-# Written by: <name here>
+# Written by: Andrew Sameh Adel Mikhail
 # Author Details: 
-#       ID: <your id here>
+#       ID: 9489
 #       Group: 3
 #       Section: 1
 #
 
 def insertion_sort(array: list) -> list:
-    return []
+    for i in range(1, len(array)):
+        # select element from unsorted side
+        key = array[i]
+        j = i - 1
+        
+        # Keep shifting between elements in sorted side
+        # to get place for selected element
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j -= 1
+        
+        # add selected element to its correct place
+        array[j + 1] = key
+    
+    return array
