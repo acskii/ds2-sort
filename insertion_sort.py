5e143ceca1deb888326776b766bae3493f50604e
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2026-02-25 12:18:55 +0200

    docs: insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -8,13 +8,17 @@
 
 def insertion_sort(array: list) -> list:
     for i in range(1, len(array)):
+        # select element from unsorted side
         key = array[i]
         j = i - 1
         
+        # Keep shifting between elements in sorted side
+        # to get place for selected element
         while j >= 0 and key < array[j]:
             array[j + 1] = array[j]
             j -= 1
         
+        # add selected element to its correct place
         array[j + 1] = key
     
     return array
