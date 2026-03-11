1ac87c0e2b8b04335a46a0233b8628904eb4b4a0
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2026-03-11 20:39:03 +0200

    feat: add quick sort

--- quick_sort.py
+++ quick_sort.py
@@ -1,7 +1,7 @@
 # QUICK SORT ALGORTIHM
-# Written by: <name>
+# Written by: Andrew Sameh Adel Mikhail
 # Author Details: 
-#       ID: <id>
+#       ID: 9489
 #       Group: 3
 #       Section: 1
 #
@@ -12,7 +12,30 @@ import random
 # NOTE: You must choose the pivot randomly. DO NOT use array[l] for example.
 # You can use random.randint(l, h) or random.choice(array) to pick the pivot
 def partition(array, l, h):
-    return -1
+    i, j = l - 1, h + 1
+    pivot = array[random.randint(l, h)]
+    
+    while True:
+        while True:
+            i += 1
+            if (array[i] >= pivot): break
+
+        while True:
+            j -= 1
+            if (array[j] <= pivot): break
+            
+        if (i >= j): break
+        
+        array[i], array[j] = array[j], array[i]
+        
+    array[j], array[l] = array[l], array[j]
+    return j
+    
+def req(array, l, h):
+    if (l < h):
+        j = partition(array, l, h)
+        req(array, l, j)
+        req(array, j+1, h)    
     
 def quick_sort(array):
-    return array
+    req(array, 0, len(array) - 1)
