c62508a7454683c23ec53f4cbb167da56d9eae9b
Ahmed Abdul Moneim <ahmedabdalmoneim9284@gmail.com>
2026-03-12 20:53:25 +0200

    merge_sort.py implemented

--- merge_sort.py
+++ merge_sort.py
@@ -1,10 +1,38 @@
-# MERGE SORT ALGORTIHM
-# Written by: <name>
+# Merge SORT ALGORTIHM
+# Written by: Ahmed Abdul Moneim
 # Author Details: 
-#       ID: <id>
+#       ID: 9284
 #       Group: 3
 #       Section: 1
 #
 
-def merge_sort(array):
-    return array
+def merge_sort(arr):
+    #base-case check##
+    if len(arr)>1:
+        mid=len(arr) //2
+        L = arr[:mid]
+        R = arr[mid:]
+        merge_sort(L)
+        merge_sort(R)
+        i=0 
+        j=0
+        k=0
+        while i<len(L) and j<len(R): #sorting the left and right lists
+            if L[i] < R[j]:
+                arr[k] = L[i] 
+                i+=1
+            else:
+                arr[k]=R[j]
+                j+=1
+            k +=1
+        # check if elements left in left
+        while i <len(L):
+            arr[k]=L[i]
+            i +=1
+            k +=1
+        #any elements left in right
+        while j <len(R):
+            arr[k]=R[j]
+            j+=1
+            k +=1
+    return arr
