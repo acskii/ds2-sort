b453b5bc1e39fafa3bfd8fae7ff7b06074b39e9d
Ahmed Abdul Moneim <ahmedabdalmoneim9284@gmail.com>
2026-03-12 21:45:59 +0200

    max_heap_sort.py implemented

--- heap_sort.py
+++ heap_sort.py
@@ -1,17 +1,30 @@
 # HEAP SORT ALGORTIHM
-# Uses max heap
-# Written by: <name>
+# Written by: Ahmed Abdul Moneim
 # Author Details: 
-#       ID: <id>
+#       ID: 9284
 #       Group: 3
 #       Section: 1
 #
 
-def max_heapify(array, i):
-    return array
+def heapify(arr,n,i):
+    largest=i
+    left = 2*i +1
+    right = 2*i +2
+    if left<n and arr[left] > arr[largest]:
+        largest=left
+    if right < n and arr[right]>arr[largest]:
+        largest =right
+    if largest!=i:
+        arr[i],arr[largest]=arr[largest],arr[i]
+        heapify(arr,n,largest)
 
-def build_max_heap(array):
-    return array
-
-def heap_sort(array):
-    return array
+def max_heap_sort(arr):
+    n = len(arr)
+    # make heap
+    for i in range(n //2-1,-1,-1):
+        heapify(arr,n, i)
+      #take element by element
+    for i in range(n -1,0,-1):
+        arr[i],arr[0]=arr[0],arr[i]
+        heapify(arr,i,0)
+    return arr
